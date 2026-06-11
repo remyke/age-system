dfff21468c1865ea3444b899263a5a20478324ec
remyke <remenyi.csaba@gmail.com>
2026-06-11 11:30:47 +0100

    Stunts settings and advencement

--- modules/sheets/ageSystemSheetItem.js
+++ modules/sheets/ageSystemSheetItem.js
@@ -145,7 +145,7 @@ export default class ageSystemItemSheet extends ItemSheet {
         }
 
         // If is is a Stunt prepare choices for stunt type
-        if(this.item.type === "stunts") {
+        if(this.item.type === "stunt") {
             data.stuntCostTypeChoices = {
                 fixed: "age-system.fixed",
                 variable: "age-system.variable"
